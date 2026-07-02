742f563d1c4ce524e521d4a5f00bdb58955d7795
Maarten Dekker <maarten.dekker@capgemini.com>
2026-07-02 16:20:37 +0200

    fix linting issues

--- components/blog/blog-menu-year.tsx
+++ components/blog/blog-menu-year.tsx
@@ -1,44 +1,47 @@
-"use client"
+"use client";
 
-import { FC, useEffect, useState } from "react";
-import styles from './blog-menu.module.css';
-import Link from "next/link";
+import { FC, useState } from "react";
 import { BlogMenuMonth } from "./blog-menu-month";
+import styles from "./blog-menu.module.css";
 import { Blog } from "./types";
 
 export type BlogMenuYearProps = {
-    blogs: Blog[],
-    slug: string,
-    year: number;
-  };
-  
+  blogs: Blog[];
+  slug: string;
+  year: number;
+};
+
 export const BlogMenuYear: FC<BlogMenuYearProps> = ({ blogs, slug, year }) => {
+  const yearBlogs = blogs.filter(
+    (b) => new Date(b.published).getFullYear() === year,
+  );
+  const hasCurrentBlog: boolean = !!yearBlogs.find((b) => b.slug === slug);
+  const months = Array.from(
+    new Set(yearBlogs.map((blog: Blog) => new Date(blog.published).getMonth())),
+  );
+  const [isOpen, setIsOpen] = useState(hasCurrentBlog);
 
-    const yearBlogs = blogs.filter(b => new Date(b.published).getFullYear() === year);
-    const hasCurrentBlog: boolean = !!yearBlogs.find(b => b.slug === slug);
-    let loopMonth: number;
-    const [isOpen, setIsOpen] = useState(hasCurrentBlog);
-
-    return (
-        <li>
-            <a id={`y${year}`} key={`y${year}`} 
-                className={`${styles.toggleLink} ${isOpen ? styles.open : styles.close}`}
-                onClick={e => setIsOpen(!isOpen)}>{year}</a>
-            <ul key={year}>
-                { yearBlogs.map((blog: Blog) => {
-
-                    const thisDate = new Date(blog.published);
-                    const thisMonth = thisDate.getMonth();
-                
-                    if (thisMonth !== loopMonth) {
-                        
-                        loopMonth = thisMonth;
-                        return <BlogMenuMonth key={thisMonth} month={thisMonth} year={year} blogs={yearBlogs} slug={slug} />
-                    }
-
-                })}
-            </ul>
-        </li>
-    )
-}
-
+  return (
+    <li>
+      <a
+        id={`y${year}`}
+        key={`y${year}`}
+        className={`${styles.toggleLink} ${isOpen ? styles.open : styles.close}`}
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        {year}
+      </a>
+      <ul key={year}>
+        {months.map((month) => (
+          <BlogMenuMonth
+            key={month}
+            month={month}
+            year={year}
+            blogs={yearBlogs}
+            slug={slug}
+          />
+        ))}
+      </ul>
+    </li>
+  );
+};
